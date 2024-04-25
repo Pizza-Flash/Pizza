d0eb5bc086002c468a8afe6ea4717b70efcf9f13
Pizza-Flash <161605672+Pizza-Flash@users.noreply.github.com>
2024-04-25 18:10:59 +0200

    d

--- src/components/checkout/checkout.component.ts
+++ src/components/checkout/checkout.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { WarenkorbComponent } from '../warenkorb/warenkorb.component';
 import { LieferadresseFormComponent } from '../lieferadresse.form/lieferadresseForm.component';
-import { LieferadresseListComponent } from '../lieferadresse.list/lieferadresseList.component';
+
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   standalone: true,
   styleUrls: ['./checkout.component.css'],
-  imports:[WarenkorbComponent,LieferadresseFormComponent,LieferadresseListComponent]
+  imports:[WarenkorbComponent,LieferadresseFormComponent]
 })
 export class CheckoutComponent implements OnInit {
 
--- src/components/lieferadresse.form/lieferadresseForm.component.ts
+++ src/components/lieferadresse.form/lieferadresseForm.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {LieferadresseListComponent} from "../lieferadresse.list/lieferadresseList.component";
+
 import {Adresse} from "../../models/adresse";
 import {Kunde} from "../../models/kunde";
 import {KundenService} from "../../services/kunden.service";
@@ -36,5 +36,6 @@ export class LieferadresseFormComponent implements OnInit {
     this.kunde.adresse = new Adresse(null, null, null, null);
 
     this.kundenService.saveKunde(this.kunde);
+    
   }
 }
